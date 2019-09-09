48094e5e4c358a25be6d3a4a57d34747cdf04886
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-09-09 19:35:41 +0200

    Initial commit SocketServer get/post request

---
+++ src/server/demo1/Server.java
@@ -0,0 +1,55 @@
+package server.demo1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private ServerSocket server;
+
+    public static void main(String[] args) throws IOException {
+       Server server = new Server();
+       server.start();
+    }
+
+    //Start server
+    public void start(){
+        try {
+            server = new ServerSocket(8888);
+
+            this.receive();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Receive client
+    private void receive(){
+        try {
+            Socket client = server.accept();
+            //接收客户端请求信息
+            StringBuilder message = new StringBuilder("");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String s = null;
+            while((s=reader.readLine()).length()>0){
+                message.append(s);
+                message.append("\r\n");
+                if(null == s)
+                    break;
+            }
+            //接收到了客户端的信息
+            String requestInfo = message.toString().trim();
+            System.out.println("Message from client is: "+requestInfo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Stop server
+    public void stop(){
+
+    }
+}
---
+++ src/server/demo1/Server2.java
@@ -0,0 +1,53 @@
+package server.demo1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server2 {
+    private ServerSocket server;
+
+
+        public static void main(String[] args) throws IOException {
+            Server2 server = new Server2();
+            server.start();
+        }
+
+        //Start server
+        public void start(){
+            try {
+                server = new ServerSocket(8888);
+
+                this.receive();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Receive client
+        private void receive(){
+            try {
+                Socket client = server.accept();
+                //接收客户端请求信息
+                StringBuilder message = new StringBuilder("");
+                String s = null;
+                byte[] data = new byte[20480];
+                int load = client.getInputStream().read(data);
+
+                //接收到了客户端的信息
+                String requestInfo = new String(data,0,load).trim();
+                System.out.println("Message from client is: "+requestInfo);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Stop server
+        public void stop(){
+
+        }
+
+}
